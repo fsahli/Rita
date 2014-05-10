db7512bcfa6cffe8e3187ce29a1669fda14d52f0
Francisco Sahli <francisco.sahli@gmail.com>
2014-05-10 19:51:06 -0400

    First working version

--- Rita.py
+++ Rita.py
@@ -6,14 +6,21 @@ from Tkinter import *
 
 import time
 import RPi.GPIO as GPIO
+
+green_pin = 17
+white_pin = 23
+red_pin = 25
  
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(green_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(white_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(red_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 ASCII_i=65
 ASCII_f=90
 gap = 150
 current_char = ASCII_i
+words=""
 
 def char_dif(char_n,increment):
 	new_char=char_n+increment
@@ -26,15 +33,49 @@ def char_dif(char_n,increment):
 
 def check_button():
 	global current_char
-	if (GPIO.input(24) == GPIO.HIGH):
-		while (GPIO.input(24) == GPIO.HIGH):
+	global words
+	if (GPIO.input(red_pin) == GPIO.LOW):
+		counter = 0
+		while (GPIO.input(red_pin) == GPIO.LOW):
 			time.sleep(0.01)
+			counter+=1
+			print counter
 		print "Button Pressed."
-		current_char=current_char+1
-		for i in range(len(letters)):
-			w.itemconfig(letters[i],text=chr(char_dif(current_char,i-2)))		
-  	master.after(10,check_button)
+		if counter<200:
+			print "entreif"
+			current_char=char_dif(current_char,1)
+			for i in range(len(letters)):
+				w.itemconfig(letters[i],text=chr(char_dif(current_char,i-2)))		
+		else:
+			print "entrelese"
+			words+=" "
+			w.itemconfig(words_display,text=words)		
+
  
+	if (GPIO.input(white_pin) == GPIO.LOW):
+		counter = 0
+		while (GPIO.input(white_pin) == GPIO.LOW):
+			time.sleep(0.01)
+			counter+=1
+		if counter<300:
+			words+=chr(current_char)
+			w.itemconfig(words_display,text=words)		
+		else:
+			words=""
+			w.itemconfig(words_display,text=words)		
+	if (GPIO.input(green_pin) == GPIO.LOW):
+		counter = 0
+		while (GPIO.input(green_pin) == GPIO.LOW):
+			time.sleep(0.01)
+			counter+=1
+		if counter<300:
+			current_char=char_dif(current_char,-1)
+			for i in range(len(letters)):
+				w.itemconfig(letters[i],text=chr(char_dif(current_char,i-2)))		
+		else:
+			words=words[0:-1]
+			w.itemconfig(words_display,text=words)		
+	master.after(10,check_button)
 
 master = Tk()
 
@@ -52,6 +93,7 @@ for i in range(5):
 
 #w.itemconfig(t,text='B')
 
+words_display=w.create_text(master.winfo_screenwidth()/2,master.winfo_screenheight()/2,text=words,font=("Times",80),fill='white',width=master.winfo_screenwidth(),anchor='n')
 
 master.after(10,check_button)
 master.mainloop()
